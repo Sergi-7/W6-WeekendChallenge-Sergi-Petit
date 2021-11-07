420b0066bd66f20523714661afce353575d9e6cf
Sergi Petit <sergi_hak_7@hotmail.com>
2021-11-07 18:34:20 +0100

    Added test for deleteRobot action and reducer

--- src/redux/reducers/robotsReducer.test.js
+++ src/redux/reducers/robotsReducer.test.js
@@ -1,5 +1,8 @@
 import RobotList from "../../components/RobotList/RobotList";
-import { loadRobotsAction } from "../actions/actionsCreators";
+import {
+  deleteRobotAction,
+  loadRobotsAction,
+} from "../actions/actionsCreators";
 import actionTypes from "../actions/actionTypes";
 import robotsReducer from "./robotsReducer";
 
@@ -33,6 +36,38 @@ describe("Given a robotsReducer", () => {
       const newList = robotsReducer(initialRobotsList, action);
 
       expect(newList).toEqual(robotsList);
+    });
+  });
+  describe("When it receives a deleteRobot action", () => {
+    test("Then it should return a list of robots without the specified id", () => {
+      const robotsList = [
+        {
+          name: "santi",
+          image: "santi.dot",
+          id: 1,
+          stats: {
+            speed: 3,
+            stamina: 3,
+            date: "131313",
+          },
+        },
+        {
+          name: "santi",
+          image: "santi.dot",
+          id: 2,
+          stats: {
+            speed: 3,
+            stamina: 3,
+            date: "131313",
+          },
+        },
+      ];
+      const deletedRobot = robotsList[0];
+
+      const action = deleteRobotAction(deletedRobot.id);
+      const newRobotsList = robotsReducer(robotsList, action);
+
+      expect(newRobotsList).not.toContain(deletedRobot);
     });
   });
 });
